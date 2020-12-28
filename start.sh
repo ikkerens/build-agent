fe3fb0961f1f200a3de3808008a7efc0405d0bed
Mark Pross <incapable@users.noreply.github.com>
2020-12-28 23:49:02 +0100

    Updated the testing database password

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo /etc/init.d/postgresql "start"
 sudo -u postgres psql -c "CREATE DATABASE cowspiracy_test;"
-sudo -u postgres psql -c "CREATE USER testing WITH PASSWORD 'example'";
+sudo -u postgres psql -c "CREATE USER testing WITH PASSWORD 'testing'";
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE \"cowspiracy_test\" to testing;";
