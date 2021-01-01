782470f01844a283d60a27ca986f0d6e0211d883
Rens Rikkerink <1952177+ikkerens@users.noreply.github.com>
2021-01-01 16:39:06 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/postgresql "start"
 sudo -u postgres psql -c "CREATE DATABASE cowspiracy_test;"
 sudo -u postgres psql -c "CREATE USER testing WITH PASSWORD 'testing'";
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE \"cowspiracy_test\" to testing;";
+/run-services.sh
